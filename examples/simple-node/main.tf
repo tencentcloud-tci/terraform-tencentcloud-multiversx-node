f190411b0b3230cfe21227c4bca7cc5942f24a4a
ritchiecai <ritchiecai@tencent.com>
2023-04-11 20:15:33 +0800

    chort: update example

--- examples/simple-node/main.tf
+++ examples/simple-node/main.tf
@@ -1,5 +1,5 @@
 module "observer" {
-  source          = "./modules/multiversx-observer"
+  source          = "git@github.com:ritch2022/terraform-tencentcloud-multiversx-lighthouse.git"
   az              = "eu-frankfurt-1"
   instance_name   = "mx-light"
   deployment_mode = "single"
