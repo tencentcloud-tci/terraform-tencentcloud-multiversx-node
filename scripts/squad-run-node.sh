4f328ad477c40712913ab7385a4dd3b337fc546a
Tudor Toma <tudortoma80@gmail.com>
2023-07-24 09:26:42 +0200

    suppres output

--- scripts/squad-run-node.sh
+++ scripts/squad-run-node.sh
@@ -269,10 +269,14 @@ remove_default_firewall_rules() {
 }
 
 run_cis_hardening() {
-    sudo yum -y install epel-release
-    sudo yum -y install ansible
-    sudo yum -y install git
-    sudo yum -y update nss
+    echo "===== Installing epel-release ... ====="
+    sudo yum -y -q install epel-release
+    echo "===== Installing ansible ... ====="
+    sudo yum -y -q install ansible
+    echo "===== Installing git ... ====="
+    sudo yum -y -q install git
+    echo "===== Installing nss ... ====="
+    sudo yum -y -q update nss
     git clone https://github.com/ritch2022/terraform-tencentcloud-multiversx-lighthouse.git /home/lighthouse/source-repo/
     cd /home/lighthouse/source-repo/
     sudo ansible-playbook --connection=local --inventory 127.0.0.1 /home/lighthouse/source-repo/scripts/cis-hardening/cis.yml
