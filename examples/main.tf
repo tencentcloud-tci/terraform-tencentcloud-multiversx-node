7bd0d332891f7eb4c832c1a5cf2429a217f2c931
Tudor Toma <tudortoma80@gmail.com>
2023-12-12 23:27:58 +0100

    adding support for testnet/devnet

--- examples/main.tf
+++ examples/main.tf
@@ -4,16 +4,16 @@ module "multiversx-observer" {
   #source = "tencentcloud-tci/multiversx-node/tencentcloud" #terraform module published in the registry
   source = "/Users/tudortoma/Documents/Projects/terraform-tencentcloud-multiversx-node"
   #-------basic variables
-  az            = "eu-frankfurt-1" #availability zone to deploy
+  az            = "eu-frankfurt-1"       #availability zone to deploy
   instance_name = "mvx-observer-testnet" #name of the LH instance
   blueprint_id  = "lhbp-a7oxy3em"
 
   #-------deployment variables
   deployment_mode = "db-lookup-hdd" #the deployment mode: lite, db-lookup-hdd, db-lookup-ssd
-  purchase_period = 1      #the valability of the purchase, in months
+  purchase_period = 1               #the valability of the purchase, in months
 
   #-------State specific variables
-  need_tat_commands = false      #set 'false' only if the commands are already deployed (if previous/paralel deployment existed)
+  need_tat_commands = false     #set 'false' only if the commands are already deployed (if previous/paralel deployment existed)
   network           = "testnet" #choose between mainnet, testnet, devnet
 
   #-------firewall details
@@ -29,8 +29,8 @@ module "multiversx-observer" {
   }]
   #-------disk variables for db-lookup option
   #leave default unless disk becomes full
-  cbs0_disk_size = 50 #disk contains node-0 and node-metachain data
-  cbs1_disk_size = 50 #disk contains node-1
-  cbs2_disk_size = 50 #disk contains node-2
-  floating_cbs = "lhdisk-c0wn1l0m" #ID of the floater disk which will be used to download and extract the node DB history
+  cbs0_disk_size = 50                #disk contains node-0 and node-metachain data
+  cbs1_disk_size = 50                #disk contains node-1
+  cbs2_disk_size = 50                #disk contains node-2
+  floating_cbs   = "lhdisk-c0wn1l0m" #ID of the floater disk which will be used to download and extract the node DB history
 }
