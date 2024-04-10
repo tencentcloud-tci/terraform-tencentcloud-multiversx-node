7114b6d3e7d365de63334f7f1befbae5066dfd9e
Tudor Toma <tudortoma80@gmail.com>
2024-04-10 09:43:12 +0200

    change source dbs

--- examples/main.tf
+++ examples/main.tf
@@ -3,12 +3,14 @@ module "multiversx-observer" {
   #-------source repo
   #source = "tencentcloud-tci/multiversx-node/tencentcloud" #terraform module published in the registry
   source = "/Users/tudortoma/Documents/Projects/terraform-tencentcloud-multiversx-node" #terraform module published in the registry
+  
   #-------basic variables
   az            = "eu-frankfurt-1"      #availability zone to deploy
   instance_name = "mvx-observer-test" #name of the LH instance
   blueprint_id  = "lhbp-a7oxy3em"
   bundle_id     = "bundle_ent_lin_02" #if performance is not a concern you can also use the 'bundle2022_gen_lin_05' (2core/8GB) for testnet/devnet only 
   #for more information please refer to MultiversX system requirements: https://docs.multiversx.com/validators/system-requirements
+  
   #-------deployment variables
   deployment_mode = "db-lookup-hdd" #the deployment mode: lite, db-lookup-hdd, db-lookup-ssd
   purchase_period = 1               #the valability of the purchase, in months
